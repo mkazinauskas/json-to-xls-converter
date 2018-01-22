55b9a8a3974dba437a9853a960c7de06ee0e14f9
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-01-22 13:16:17 +0200

    Initial setup

---
+++ src/main/java/com/modzo/jsonxlsconverter/JsonxlsconverterApplication.java
@@ -0,0 +1,12 @@
+package com.modzo.jsonxlsconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonxlsconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonxlsconverterApplication.class, args);
+	}
+}
